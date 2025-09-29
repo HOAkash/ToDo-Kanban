93874f33ada097c13f6900570bb30630c3d4be11
HOAkash <akasha1664@gmail.com>
2025-09-29 17:16:00 +0530

    v 0.1

--- src/components/boardReducer.js
+++ src/components/boardReducer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const initialBoard = {
   columns: {
-    todo: { id: "todo", title: "To Do", tasks: ["Task 1", "Task 2"] },
+    todo: { id: "todo", title: "To Do", tasks: [] },
     progress: { id: "progress", title: "In Progress", tasks: [] },
     done: { id: "done", title: "Done", tasks: [] }
   },
